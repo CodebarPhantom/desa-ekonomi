ce1f8fe5cf6525d8418a9651a165a21cf2f42579
CodebarPhantom <eryangamer10@gmail.com>
2023-02-14 14:57:37 +0700

    feat: public for frontend

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Auth::routes([
 ]);
 
 Route::get('/', function () {
-        return view('welcome');
+        return view('layouts.user.index');
     });
 
 Route::middleware(['auth'])->group(function() {
